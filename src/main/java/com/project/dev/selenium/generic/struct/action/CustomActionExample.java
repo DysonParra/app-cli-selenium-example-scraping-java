29fbc9c7049cd3a791a34cb6d5a859c237b5b675
unknown <dysontilano@gmail.com>
2026-04-10 15:23:45 -0500

    feat: selenium-generic-lib updated: Added support to save script results and use them in other actions

--- src/main/java/com/project/dev/selenium/generic/struct/action/CustomActionExample.java
+++ src/main/java/com/project/dev/selenium/generic/struct/action/CustomActionExample.java
@@ -60,11 +60,14 @@ public class CustomActionExample extends Action {
      * @param driver   es el driver del navegador.
      * @param element  es el {@code WebElement} que se le va a ejecutar dicha acción.
      * @param flagsMap contiene las {@code Flag} pasadas por consola.
+     * @param scriptResults
      * @return {@code true} si se ejecuta la acción correctamente.
      * @throws Exception si ocurre algún error ejecutando la acción indicada.
      */
     @Override
-    public boolean executeAction(@NonNull WebDriver driver, @NonNull WebElement element, Map<String, String> flagsMap) throws Exception {
+    public boolean executeAction(@NonNull WebDriver driver, @NonNull WebElement element, Map<String, String> flagsMap, List<Object> scriptResults) throws Exception {
+        outputPath = Action.assignScriptResult(outputPath, scriptResults);
+        outputFileName = Action.assignScriptResult(outputFileName, scriptResults);
         System.out.println("        Current page: " + driver.getCurrentUrl());
         new File(outputPath).mkdirs();
         boolean result;
